1a9e7b90be86adad4b096bf236852d17d57926e3
André Cano <andrerenatofc@gmail.com>
2023-03-16 11:19:41 -0300

    Code Structuring 2

--- functions.h
+++ functions.h
@@ -6,5 +6,6 @@ void mergeSort(int *array, int length);
 void quickSort(int *array, int length);
 int binarySearch(int *array, int value, int length);
 int ternarySearch(int *array, int value, int length);
+void randomArray(int *array, int length, int bottom, int top);
 
 #endif
