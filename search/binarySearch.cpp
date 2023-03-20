f0f7ef2e811700aa2914b8f445af5a347f35829e
André Cano <andrerenatofc@gmail.com>
2023-03-20 17:51:55 -0300

    Header Search

--- search/binarySearch.cpp
+++ search/binarySearch.cpp
@@ -1,11 +1,11 @@
+int bSearch(int *array, int value, int start, int end)
+{
+    // TODO: implement me
+    return -1;
+}
 
 int binarySearch(int *array, int value, int length)
 {
     return bSearch(array, value, 0, length);
 }
 
-int bSearch(int *array, int value, int start, int end)
-{
-    // TODO: implement me
-    return -1;
-}
