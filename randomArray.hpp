cb2a5ff18254bbf0990ab1e184fe58dd6e12c16d
LuisFelipeMarrote <luisfelipe.marrote@gmail.com>
2023-03-22 20:52:01 -0300

    Pequeno ajuste no randomizador de números.

--- randomArray.hpp
+++ randomArray.hpp
@@ -3,11 +3,13 @@
 #include <ctime>
 #include <random>
 
+#include <initializer_list>
+
 void randomArray(int **array, int length, int floor, int celling)
 {
-    random_device rd;   // non-deterministic generator
-    mt19937 gen(rd());  // to seed mersenne twister.
-    uniform_int_distribution<> dist(20,2000000); // distribute results between 20 and 2000000 inclusive
+    std::random_device rd;   // non-deterministic generator
+    std::mt19937 gen(rd());  // to seed mersenne twister.
+    std::uniform_int_distribution<int> dist(20,2000000); // distribute results between 20 and 2000000 inclusive
     *array = new int[length];
     srand(time(0));
     for (int i = 0; i < length; i++)
